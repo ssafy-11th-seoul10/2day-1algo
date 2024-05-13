b1ba6e7e3e18292ec7345ab578e0725aac0aa36d
홍창기 <hckhong@naver.com>
2024-05-13 22:52:30 +0900

    feat(홍창기): 샘플파일 업로드

---
+++ week01_240513/홍창기/BOJ_1000_240513_example.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BOJ_1000_240513_example {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+
+        sc.close();
+    }
+}
