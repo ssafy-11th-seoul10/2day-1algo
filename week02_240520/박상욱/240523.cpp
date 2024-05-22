d2ff5c1df90ede426eaf07ceb01eaf4ee8e5db70
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-05-23 00:07:35 +0900

    박상욱_240523_BOJ_24313

---
+++ week02_240520/박상욱/240523.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    int a1,a0, c, n0;
+    cin >> a1>>a0>>c>>n0;
+    
+    if(a1*n0 + a0 <= c*n0 && a1<=c){
+        cout << 1 << "\n";
+    }
+    else{
+        cout << 0 << "\n";
+    }
+    
+    return 0;
+}
