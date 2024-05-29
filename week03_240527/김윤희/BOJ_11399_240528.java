6a91f102b8c73841e636c56c23b661ef6b99f103
홍창기 <hckhong@naver.com>
2024-05-29 19:15:45 +0900

    김윤희_240528_백준_11399

--- week03_240527/김윤희/BOJ_11399_240528.java
+++ week03_240527/김윤희/BOJ_11399_240528.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class BOJ_5430 {
+public class BOJ_11399 {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
