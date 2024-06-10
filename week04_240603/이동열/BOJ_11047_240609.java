b997fcb2d07785cb8d279960db93f3702ff231f6
홍창기 <hckhong@naver.com>
2024-06-10 10:14:58 +0900

    이동열_240609_백준_11047

--- week04_240603/이동열/BOJ_11047_240609.java
+++ week04_240603/이동열/BOJ_11047_240609.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ11407 {
+public class BOJ11047 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
