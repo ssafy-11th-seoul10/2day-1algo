01f01979d8aa37dd79c015b0e4ec844d1c927ea9
Kyungrim Park <79007826+g16rim@users.noreply.github.com>
2024-06-21 17:39:30 +0900

    박경림_240621_BOJ_11055

---
+++ week06_240617/박경림/BOJ_240621_11055.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = arr[i];
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result = Math.max(result, dp[i]);
+        }
+
+        System.out.println(result);
+    }
+}
