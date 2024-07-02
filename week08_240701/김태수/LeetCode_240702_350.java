e28219b35b41705ba29e2909a1ca2d4d14a26ec4
kimtaesoo99 <kimtaesoo7@naver.com>
2024-07-02 20:04:23 +0900

    김태수_240702_리트코드_350

---
+++ week08_240701/김태수/LeetCode_240702_350.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        List<Integer> list = new ArrayList<>();
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : nums2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                list.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
+
+        return list.stream()
+            .mapToInt(i -> i)
+            .toArray();
+    }
+}
