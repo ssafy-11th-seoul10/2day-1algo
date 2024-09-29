a8d8a0a06611ccf5ee8837c45ced32a2c40f994e
홍창기 <hckhong@naver.com>
2024-09-29 18:27:45 +0900

    홍창기_240928_백준_32404

---
+++ week20_240923/홍창기/BOJ_240928_32404.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define size(v) (int)v.size()
+#define all(v) v.begin(),v.end()
+#define setw(n, c) cout << setw(n) << setfill(c);
+#define setp(n) cout << fixed << setprecision(n);
+#define printw(x) cout << (x) << ' ';
+#define println(x) cout << (x) << '\n';
+
+#ifdef BOJ
+#define testPrint(x) ((void)0)
+#else
+#define testPrint(x) cout << "[D] " << #x << ':' << x << '\n'
+#endif
+
+using namespace std;
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+
+const double PI = M_PI;
+
+int main() {
+    FASTIO;
+
+    int n;
+    cin >> n;
+
+    int a = n / 2 + 1;
+    int d = n / 2;
+    int i = 0;
+
+    while (i < n) {
+        printw(a);
+        a++;
+        i++;
+
+        if (i < n) {
+            printw(d);
+            d--;
+            i++;
+        }
+    }
+
+    cout << '\n';
+
+    return 0;
+}
