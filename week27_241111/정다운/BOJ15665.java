7d6bc9e6d5884b715c33aefe5702653d0c548269
unknown <wjdek88@naver.com>
2024-11-14 21:02:00 +0900

    정다운_241114_백준_15665&15666

---
+++ week27_241111/정다운/BOJ15665.java
@@ -0,0 +1,60 @@
+package baekjoon;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ15665 {
+
+    static int N, M;
+    static int[] arr;
+
+    static int[] perm;
+    static boolean[] visited;
+
+    static StringBuilder sb = new StringBuilder();;
+
+    public static void permutation(int depth) {
+
+        if(depth == M) {
+            for(int i = 0; i < M; i++)
+                sb.append(perm[i]).append(" ");
+
+            sb.append("\n");
+
+            return;
+        }
+
+        int flag = 0;
+
+        for(int i = 0; i < N; i++) {
+            if(flag == arr[i])
+                continue;
+
+            perm[depth] = arr[i];
+            flag = arr[i];
+            permutation(depth + 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        perm = new int[M];
+        visited = new boolean[N];
+
+        permutation(0);
+        System.out.println(sb.toString());
+    }
+}
